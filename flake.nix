c476adac61d694e1e4c6254f4a93859263ac01a0
Kaitlyn Kenwell <redrield@gmail.com>
2026-03-25 19:08:27 -0400

    Set up dev env

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs@{ flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+
+        };
+      };
+    };
+}
